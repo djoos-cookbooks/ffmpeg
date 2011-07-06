b7c409e5f99e1c4dcd71a9ff2aacc85414934456
Jamie Winsor <jamie@enmasse.com>
2011-07-06 15:43:12 -0700

    Initial public release of FFMPEG cookbook

---
+++ attributes/default.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: ffmpeg
+# Attributes:: default
+#
+# Author:: Jamie Winsor (<jamie@enmasse.com>)
+#
+# Copyright 2011, En Masse Entertainment, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default[:ffmpeg][:install_method] = :source
+default[:ffmpeg][:prefix] = "/usr/local"
+default[:ffmpeg][:git_repository] = "git://git.videolan.org/ffmpeg.git"
+default[:ffmpeg][:compile_flags] = [
+  "--disable-debug",
+  "--enable-pthreads",
+  "--enable-nonfree",
+  "--enable-gpl",
+  "--disable-indev=jack",
+  "--enable-libx264",
+  "--enable-libfaac",
+  "--enable-libmp3lame",
+  "--enable-libtheora",
+  "--enable-libvorbis",
+  "--enable-libvpx",
+  "--enable-libxvid",
+  "--enable-libfaad"
+]
+
+# JW 07-06-11: Hash of commit or a HEAD should be used - not a tag. Sync action of Git
+# provider will always attempt to update the git clone if a tag is used.
+default[:ffmpeg][:git_revision]   = "ace432f62cdcedf812e7c4d77fc5b03322170fa8" # 0.6.3
+
+default[:ffmpeg][:libvpx][:revision] = "0491c2cfc89cded04de386ae691654c7653aac9b"
+default[:ffmpeg][:x264][:revision] = "stable"
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "En Masse Entertainment"
+maintainer_email "jamie@enmasse.com"
+license          "Apache 2.0"
+description      "Installs and configures FFMPEG from source or package"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+supports "ubuntu", "10.04"
+
+depends "x264", "~> 0.1.1"
+depends "libvpx", "~> 0.1.1"
+depends "build-essential"
+depends "git"
