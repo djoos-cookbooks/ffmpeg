82cb920fa3ae7b2c7bee5e9c7652c3a2d9a8f6f9
Jamie Winsor <jamie@enmasse.com>
2011-11-07 12:49:36 -0800

    Bump required versions for libvpx and x264

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,11 @@ maintainer_email "jamie@enmasse.com"
 license          "Apache 2.0"
 description      "Installs and configures FFMPEG from source or package"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          "0.2.0"
 
 supports "ubuntu", "10.04"
 
-depends "x264", "~> 0.1.1"
-depends "libvpx", "~> 0.1.1"
+depends "x264", "~> 0.2.0"
+depends "libvpx", "~> 0.2.0"
 depends "build-essential"
 depends "git"
