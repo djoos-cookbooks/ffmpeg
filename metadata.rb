e623009e4045278a4fbf94b48ad739f9916f4f71
Jamie Winsor <jamie@vialstudios.com>
2012-05-23 12:12:03 -0700

    bump version 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs and configures FFMPEG from source or package"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 supports "ubuntu", "10.04"
 
