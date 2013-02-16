d02940d20a404f4e17f5f9f27f49a670b4d5dd2e
Iuri Aranda <iuri.aranda@uwhisp.com>
2013-02-16 19:20:32 +0100

    Add cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "En Masse Entertainment"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
+name             "ffmpeg"
 description      "Installs and configures FFMPEG from source or package"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
