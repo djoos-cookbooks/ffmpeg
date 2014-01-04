c7350253d657283a9edc881cc3a169eb61a63530
David Joos <david.joos@gmail.com>
2014-01-04 00:55:28 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "development@davidjoos.com"
 license          "MIT"
 description      "Installs/Configures FFMPEG"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.3.1"
 
 supports "ubuntu"
 
