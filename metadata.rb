0d10e3a4a0d904a9727786fd68099726c33e0c8e
ianrossi <ian.d.rossi@aimtheory.com>
2014-02-21 12:01:35 -0500

    Bumped version and added redhat, centos to metadata

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,10 @@ maintainer_email "development@davidjoos.com"
 license          "MIT"
 description      "Installs/Configures FFMPEG"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.3.2"
 
 supports "ubuntu"
+supports "redhat", "centos"
 
 depends "x264"
 depends "libvpx"
