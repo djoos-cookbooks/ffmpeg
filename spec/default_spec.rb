b40aa1edafaa405ba31d3b674b6de5ed201b1886
Lloyd Philbrook <lloyd@tablexi.com>
2014-04-09 15:15:29 +0700

    add basic tests

---
+++ spec/default_spec.rb
@@ -0,0 +1,19 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+describe 'ffmpeg::default' do
+  let(:chef_run) { ChefSpec::Runner.new }
+
+  it 'include ffmpeg::source recipe by default' do
+    chef_run.converge(described_recipe)
+    expect(chef_run).to include_recipe('ffmpeg::source')
+    expect(chef_run).not_to include_recipe('ffmpeg::package')
+  end
+
+  it 'include ffmpeg::package if attribute changed' do
+    chef_run.node.set['ffmpeg']['install_method'] = :package
+    chef_run.converge(described_recipe)
+    expect(chef_run).to include_recipe('ffmpeg::package')
+    expect(chef_run).not_to include_recipe('ffmpeg::source')
+  end
+end
