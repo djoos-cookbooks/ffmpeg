31a46cf626733c9b88acd1adeaf18db97ec2202c
David Joos <david.joos@gmail.com>
2014-08-04 20:58:05 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures FFMPEG'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.1'
+version '0.4.2'
 
 supports 'ubuntu'
 
