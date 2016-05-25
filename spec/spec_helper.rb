fd7ea272ed611f5f179dd011519b72429dc3c5b3
Lloyd Philbrook <lloyd@tablexi.com>
2016-05-25 17:40:10 -0500

    update spec_helper with platform and path to libs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
+
 at_exit { ChefSpec::Coverage.report! }
+
+RSpec.configure do |config|
+  config.platform = 'ubuntu'
+  config.version = '14.04'
+end
